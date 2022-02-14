787eaf0924db47a81d5c8a8d9d895e4c5bd3a243
Tianyi Zhang <tianyizhang1018@gmail.com>
2022-02-13 19:08:10 -0800

    list in progress 2

--- dev/app/src/main/java/com/example/softwair_dev/Data_display.java
+++ dev/app/src/main/java/com/example/softwair_dev/Data_display.java
@@ -3,14 +3,17 @@ package com.example.softwair_dev;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -26,9 +29,7 @@ public class Data_display extends AppCompatActivity {
     private String BASE_URL = "http://10.0.2.2:3000";
 
     ListView l;
-    String paramteres[] = { "ID", "Name",
-            "Temp", "E_PM",
-            "S_PM", "RH" };
+    private static ArrayList<String>  data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,52 +42,66 @@ public class Data_display extends AppCompatActivity {
 
         retrofitInterface = retrofit.create(RetrofitInterface.class);
 
+
         l = findViewById(R.id.list);
+        data = new ArrayList<>();
+        data.add("Please Refresh");
         ArrayAdapter<String> arr;
-        arr = new ArrayAdapter<String>(this,
+        arr = new ArrayAdapter<String> (this,
                 androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
-                paramteres);
+                data);
         l.setAdapter(arr);
 
 
+
         findViewById(R.id.button_refresh).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                handRefreshDialog();
-            }
-        });
-    }
-    private void handRefreshDialog(){
-
-        View view = getLayoutInflater().inflate(R.layout.activity_data_display, null);
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-        builder.setView(view).show();
-
-        Button refresh_button = view.findViewById(R.id.button_refresh);
-
-        refresh_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 Call<LatestRecord> call = retrofitInterface.executeQueryLatest();
-
                 call.enqueue(new Callback<LatestRecord>() {
                     @Override
                     public void onResponse(Call<LatestRecord> call, Response<LatestRecord> response) {
-
                         if (response.code() == 200) {
-                            update();
+                            arr.notifyDataSetChanged();
+                            Toast.makeText(Data_display.this, "Data retrived",
+                                    Toast.LENGTH_LONG).show();
+                            String s = response.body().getRecord();
+                            System.out.println(s);
+                            HashMap<String,String> result = null;
+                            try {
+                                result = jsonToMap(s);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            ArrayList<String> newData = new ArrayList<>();
+                            Iterator arrIterator = result.keySet().iterator();
+                            while (arrIterator.hasNext()) {
+                                String mapElement = (String) arrIterator.next();
+                                String element = mapElement + " : "+ result.get(mapElement);
+                                newData.add(element);
+                                Toast.makeText(Data_display.this, element,
+                                        Toast.LENGTH_LONG).show();
+                            }
+                            data = newData;
                         } else if (response.code() == 400) {
                             Toast.makeText(Data_display.this, "AWS query failed",
                                     Toast.LENGTH_LONG).show();
                         }
-
                     }
-                    private void update(){
-                        return;
-                    }
+                    public HashMap<String,String> jsonToMap(String t) throws JSONException {
 
+                        HashMap<String, String> map = new HashMap<String, String>();
+                        JSONObject jObject = new JSONObject(t);
+                        Iterator<?> keys = jObject.keys();
+
+                        while( keys.hasNext() ){
+                            String key = (String)keys.next();
+                            String value = jObject.getString(key);
+                            map.put(key, value);
+
+                        }
+                        return map;
+                    }
                     @Override
                     public void onFailure(Call<LatestRecord> call, Throwable t) {
                         Toast.makeText(Data_display.this, t.getMessage(),
@@ -94,9 +109,8 @@ public class Data_display extends AppCompatActivity {
                     }
                 });
 
+
             }
         });
     }
-
-
 }
